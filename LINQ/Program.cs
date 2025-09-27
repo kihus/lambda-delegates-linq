1f2da0e5c3ac4ccf2143d5a93461e1b42d03b640
Bruno Nascimento <wintergamesk@gmail.com>
2025-09-27 00:20:13 -0300

    docs: Sort lambda

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -6,9 +6,7 @@ list.Add(new Product("Notebook", 1200.00));
 list.Add(new Product("Tablet", 450.00));
 list.Add(new Product("Computador", 5550.00));
 
-Comparison<Product> comp = (p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
-
-list.Sort(comp);
+list.Sort((p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper()));
 
 foreach(var item in list)
 {
