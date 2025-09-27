1b484b4e2cd5085b2d5aa443093638ea6ca2bfd6
Bruno Nascimento <wintergamesk@gmail.com>
2025-09-27 00:18:51 -0300

    docs: Comparison lambda

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -6,16 +6,11 @@ list.Add(new Product("Notebook", 1200.00));
 list.Add(new Product("Tablet", 450.00));
 list.Add(new Product("Computador", 5550.00));
 
-Comparison<Product> comp = CompareProducts;
+Comparison<Product> comp = (p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
 
-list.Sort(CompareProducts);
+list.Sort(comp);
 
 foreach(var item in list)
 {
 	Console.WriteLine(item);
-}
-
-static int CompareProducts(Product p1, Product p2)
-{
-	return p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
 }
