ba0a4c7249a19a443ea84d66dd8c559dd79f81fe
Bruno Nascimento <wintergamesk@gmail.com>
2025-09-27 00:05:20 -0300

    bad solution

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using LINQ.Entities;
+
+List<Product> list = new();
+list.Add(new Product("TV", 900.00));
+list.Add(new Product("Notebook", 1200.00));
+list.Add(new Product("Tablet", 450.00));
+
+list.Sort();
+
+foreach(var item in list)
+{
+	Console.WriteLine(item);
+}
