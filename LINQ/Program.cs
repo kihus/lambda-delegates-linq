cfd0e85fa794d67600998114a7b0a097e0bcb450
Bruno Nascimento <wintergamesk@gmail.com>
2025-09-27 00:18:10 -0300

    docs: Comparison with instance

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -6,6 +6,8 @@ list.Add(new Product("Notebook", 1200.00));
 list.Add(new Product("Tablet", 450.00));
 list.Add(new Product("Computador", 5550.00));
 
+Comparison<Product> comp = CompareProducts;
+
 list.Sort(CompareProducts);
 
 foreach(var item in list)
